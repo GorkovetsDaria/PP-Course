614477b6b34c3d52c878f58a52fa1c477b4576b8
Горковец Дарья <gorkovetsdaria@MacBook-Air-Gorkovec.local>
2024-12-15 15:28:18 +0100

    lesson-3

---
+++ lesson-3/main.js
@@ -0,0 +1,13 @@
+const titleProject = 'Оценка стоимости';
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 2000;
+let percentage = 10;
+let fullPrice = 80000;
+let responsive = true;
+
+console.log(titleProject);
+console.log(screensValue);
+console.log(screenPrice);
+console.log(percentage);
+console.log(fullPrice);
+console.log(responsive);
